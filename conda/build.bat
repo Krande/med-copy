f843fd82d53c7b5a103753bca0cbe91f431cae60
krande <kristoffer_andersen@outlook.com>
2024-12-15 12:36:24 +0100

    update local dev setup

---
+++ conda/build.bat
@@ -0,0 +1,90 @@
+@echo OFF
+
+setlocal ENABLEDELAYEDEXPANSION
+
+mkdir build
+cd build
+:: LLVM Flang support will have to be patched into libmed source code
+:: set FC=flang-new
+
+if not "%FC%" == "flang-new" (
+    call %RECIPE_DIR%\activate_ifx.bat
+)
+
+set TGT_BUILD_TYPE=Release
+set CFLAGS=%CFLAGS% /nologo /MD
+set CXXFLAGS=%CFLAGS% /nologo /MD /EHs
+
+if "%build_type%" == "debug" (
+    set TGT_BUILD_TYPE=Debug
+    set CFLAGS=%CFLAGS% /Od /Zi -DSWIG_PYTHON_INTERPRETER_NO_DEBUG=1
+    set CXXFLAGS=%CFLAGS% /Od /Zi -DSWIG_PYTHON_INTERPRETER_NO_DEBUG=1
+    set LDFLAGS=%LDFLAGS% /DEBUG /INCREMENTAL:NO
+    if "%FC%" == "flang-new" (
+        set FFLAGS=%FFLAGS% -g -cpp
+    ) else (
+        set FFLAGS=%FFLAGS% /Od /debug /Zi /traceback
+    )
+)
+
+:: This updates the symbols to lowercase and adds an underscore
+xcopy %RECIPE_DIR%\medfwrap_symbols.def %SRC_DIR%\src\medfwrap_symbols.def.in /Y
+
+set FFLAGS=%FFLAGS% /nologo /fpp /fixed /dll /MD /real-size:64 /integer-size:64 /assume:byterecl,aligned_dummy_args,dummy_aliases,writeable_strings
+
+echo "FFLAGS: %FFLAGS%"
+echo "CFLAGS: %CFLAGS%"
+echo "LDFLAGS: %LDFLAGS%"
+
+cmake -G "Ninja" ^
+  -D CMAKE_INSTALL_PREFIX="%PREFIX%\Library" ^
+  -D CMAKE_PROGRAM_PATH="%BUILD_PREFIX%\bin;%BUILD_PREFIX%\Scripts;%BUILD_PREFIX%\Library\bin;%PREFIX%\bin;%PREFIX%\Scripts;%PREFIX%\Library\bin" ^
+  -D CMAKE_BUILD_TYPE:STRING="%TGT_BUILD_TYPE%" ^
+  -D CMAKE_Fortran_FLAGS:STRING="%FFLAGS%" ^
+  -D CMAKE_C_FLAGS:STRING="%CFLAGS%" ^
+  -D CMAKE_CXX_FLAGS:STRING="%CXXFLAGS%" ^
+  -D CMAKE_EXE_LINKER_FLAGS:STRING="%LDFLAGS%" ^
+  -D CMAKE_VERBOSE_MAKEFILE:BOOL=ON ^
+  -D Python_FIND_STRATEGY:STRING=LOCATION ^
+  -D Python_FIND_REGISTRY:STRING=NEVER ^
+  -D Python3_ROOT_DIR:FILEPATH="%PREFIX%" ^
+  -D HDF5_ROOT_DIR:FILEPATH="%LIBRARY_PREFIX%" ^
+  -D MEDFILE_INSTALL_DOC=OFF ^
+  -D MEDFILE_BUILD_PYTHON=ON ^
+  -D MEDFILE_BUILD_TESTS=OFF ^
+  -D MEDFILE_BUILD_SHARED_LIBS=ON ^
+  -D MEDFILE_BUILD_STATIC_LIBS=OFF ^
+  -D MEDFILE_USE_UNICODE=OFF ^
+  -D MED_MEDINT_TYPE="long long" ^
+  -Wno-dev ^
+  ..
+
+if errorlevel 1 exit 1
+
+ninja
+
+if errorlevel 1 exit 1
+
+mkdir %SP_DIR%\med
+
+if errorlevel 1 exit 1
+
+ninja install
+
+if errorlevel 1 exit 1
+
+if "%build_type%" == "debug" (
+    :: Move the pdb files to the library bin directory
+    for %%f in ("%SRC_DIR%\build\src\*.pdb") do move "%%f" "%LIBRARY_BIN%"
+    for %%f in ("%SRC_DIR%\build\tools\medimport\*.pdb") do move "%%f" "%LIBRARY_BIN%"
+)
+
+copy %LIBRARY_BIN%\mdump4.exe %LIBRARY_BIN%\mdump.exe
+
+if errorlevel 1 exit 1
+
+copy %LIBRARY_BIN%\xmdump4 %LIBRARY_BIN%\xmdump
+
+if errorlevel 1 exit 1
+
+endlocal
--- conda/build.sh
+++ conda/build.sh
@@ -1,30 +1,60 @@
 set -ex
 
-export FCFLAGS="-fdefault-integer-8 ${FCFLAGS}"
-export FFLAGS="-fdefault-integer-8 ${FFLAGS}"
-export MED_MEDINT_TYPE=int
+export CLICOLOR_FORCE=1
+
+USE_MPI=OFF
+if [[ "${mpi_prefix}" == "nompi" ]]; then
+  export F77=${FC}
+  echo "Compiling for Sequential MPI=$mpi"
+else
+  USE_MPI=ON
+  echo "Compiling for MPI=$mpi"
+  export OPAL_PREFIX=$PREFIX
+  export CC=mpicc
+  export CXX=mpicxx
+  export FC=mpif90
+  export F77=mpif77
+  export F90=mpif90
+fi
+
+build_type="Release"
+if [[ "${build_type}" == "True" ]]; then
+    echo "Debugging Enabled"
+    build_type="Debug"
+    export CFLAGS="-g -O0 ${CFLAGS}"
+    export CXXFLAGS="-g -O0 ${CXXFLAGS}"
+    export FCFLAGS="-g -O0 ${FCFLAGS}"
+else
+    echo "Debugging Disabled"
+fi
+
+
+export FCFLAGS="-fdefault-integer-8 -cpp ${FCFLAGS}"
+export FFLAGS="-fdefault-integer-8 -cpp ${FFLAGS}"
+
 mkdir -p build
 pushd build
 
 # we specify both old style (all capital PYTHON)
 # and new style (Python) variables
-cmake \
+cmake -Wno-dev \
   ${CMAKE_ARGS} \
+  -D CMAKE_BUILD_TYPE=${build_type} \
   -D Python_FIND_VIRTUALENV=FIRST \
   -D Python_FIND_STRATEGY=LOCATION \
   -D Python_ROOT_DIR="${PREFIX}" \
   -D Python_EXECUTABLE="${PYTHON}" \
   -D PYTHON_EXECUTABLE="${PYTHON}" \
   -D HDF5_ROOT_DIR=${PREFIX} \
+  -D MPI_ROOT=${PREFIX} \
   -D MEDFILE_INSTALL_DOC=OFF \
   -D MEDFILE_BUILD_TESTS=OFF \
   -D MEDFILE_BUILD_PYTHON=ON \
   -D MEDFILE_BUILD_SHARED_LIBS=ON \
   -D MEDFILE_BUILD_STATIC_LIBS=OFF \
-  -D DMEDCOUPLING_USE_64BIT_IDS=ON \
   -D MEDFILE_USE_UNICODE=OFF \
   -D MED_MEDINT_TYPE=long \
-  -D SIZEOF_LONG_LONG=8 \
+  -D MEDFILE_USE_MPI=${USE_MPI} \
   ..
 
 make -j${CPU_COUNT}
